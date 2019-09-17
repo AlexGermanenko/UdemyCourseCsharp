61491cc22ff1154c24dc6d8846c9344887a2ed19
Alexander Germanenko <otmotrosok@gmail.com>
2019-09-17 14:14:30 +0700

    vars

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace ConsoleApp1
 {
@@ -6,7 +7,34 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            #region vars
+
+            int x = 1;
+            int y;
+            y = 2;
+
+            int x1 = 0;//same
+            int x2 = new int();//same
+
+            var a = 1.1;
+
+            Dictionary<string, int> dict = new Dictionary<string, int>(); // the same
+            var dict_ = new Dictionary<string, int>(); // the same
+
+            decimal money = 2.1m;
+
+            char character = 's';
+            string name = "Alex";
+
+            bool canJump = false;
+
+            object obj = 1; //can but don't
+            object obj_ = "qwerty"; //can but don't
+
+            #endregion
+            
+            Console.WriteLine($"object {obj_}");
             Console.ReadLine();
         }
     }
