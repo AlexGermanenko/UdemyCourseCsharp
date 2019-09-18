e048ac30f9d91c53e3c0905878f9a5fc806c354f
Alexander Germanenko <otmotrosok@gmail.com>
2019-09-18 16:02:00 +0700

    homeWork3_userProfile1

--- homeWork3_UserProfile/Program.cs
+++ homeWork3_UserProfile/Program.cs
@@ -9,18 +9,19 @@ namespace homeWork3_UserProfile
             string name = GetNotEmptyString("What's your name?");
             string surname = GetNotEmptyString("What's your surname?");
             int age = int.Parse(GetNotEmptyString("What's your age?"));
-            double weight = double.Parse(GetNotEmptyString("What's your weight?"));
-            double height = double.Parse(GetNotEmptyString("What's your height?"));
+            double weight = double.Parse(GetNotEmptyString("What's your weight? kg"));
+            double height = double.Parse(GetNotEmptyString("What's your height? meter"));
 
             Console.WriteLine();
 
             double IMT = Math.Round(weight / (height * height), 2);
-            
-            Console.WriteLine($"Your profile:{Environment.NewLine}" +
-                              $"Full Name: {surname}, {name}{Environment.NewLine}" +
-                              $"Age: {age}{Environment.NewLine}" +
-                              $"Weight: {weight}{Environment.NewLine}" +
-                              $"Height: {height}{Environment.NewLine}" +
+            string nl = Environment.NewLine;
+
+            Console.WriteLine($"Your profile:{nl}" +
+                              $"Full Name: {surname}, {name}{nl}" +
+                              $"Age: {age}{nl}" +
+                              $"Weight: {weight}{nl}" +
+                              $"Height: {height}{nl}" +
                               $"Body mass index: {IMT}");
 
             Console.Read();
