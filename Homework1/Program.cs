28b1f38ed98a2beafb6d0b0e03f337ae89591ba2
Alexander Germanenko <otmotrosok@gmail.com>
2019-09-18 15:10:37 +0700

    homeWork1

---
+++ Homework1/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Text;
+
+namespace Homework1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = GetNotEmptyString("What's your name?");
+
+            Console.WriteLine();
+            Console.WriteLine($"Hello, {name}!");
+
+            Console.WriteLine();
+
+            string aString = GetNotEmptyString("Input number a: ");
+            string bString = GetNotEmptyString("Input number b: ");
+
+            int aNumber = int.Parse(aString);
+            int bNumber = int.Parse(bString);
+
+            Console.WriteLine();
+            Console.WriteLine( $"Number a: {aNumber}, number b: {bNumber}");
+
+            int tempNumber = aNumber;
+            aNumber = bNumber;
+            bNumber = tempNumber;
+
+            Console.WriteLine($"Number a: {aNumber}, number b: {bNumber}");
+
+            Console.WriteLine();
+
+            aString = GetNotEmptyString("Input number: ");
+            Console.WriteLine( $"Numeral count: {aString.Length}" );
+
+            Console.Read();
+        }
+
+        private static string GetNotEmptyString(string question)
+        {
+            string result = "";
+            while (string.IsNullOrWhiteSpace(result))
+            {
+                Console.WriteLine(question);
+                result = Console.ReadLine();
+            }
+            return result;
+        }
+    }
+}
