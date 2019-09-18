507b4cf09ca9948bada99fd1a6ce16312a81f1e9
Alexander Germanenko <otmotrosok@gmail.com>
2019-09-18 15:36:23 +0700

    HomeWork3 heron formula

---
+++ HomeWork2(geron formula)/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace HomeWork2_geron_formula_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double sideA = double.Parse(GetNotEmptyString("Input side a: "));
+            double sideB = double.Parse(GetNotEmptyString("Input side b: "));
+            double sideC = double.Parse(GetNotEmptyString("Input side c: "));
+
+            double p = (sideA + sideB + sideC)/2;
+            double square = Math.Round(Math.Sqrt(p*(p-sideA)*(p - sideB)*(p - sideC)), 2);
+
+            Console.WriteLine();
+            Console.WriteLine($"S={square}");
+
+            Console.Read();
+        }
+
+        private static string GetNotEmptyString(string question)
+        {
+            string result = "";
+            while (string.IsNullOrWhiteSpace(result))
+            {
+                Console.WriteLine(question);
+                result = Console.ReadLine();
+            }
+            return result;
+        }
+    }
+}
