19fcf5cee2c2b67f7873aa5808a27bddf87c8b58
Alexander Germanenko <otmotrosok@gmail.com>
2019-09-19 17:36:13 +0700

    HomeWork8_Authentication

---
+++ HomeWork8_Authentication/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using static CommonLibrary.StringLibrary;
+
+namespace HomeWork8_Authentication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string result  = "The number of available tries have been exceeded";
+
+            for (int i = 0; i < 3; i++)
+            {
+                string login = GetNotEmptyString("Enter login: ");
+                string password = GetNotEmptyString("Enter password: ");
+
+                if (login == "Alexander" && password == "qwerty")
+                {
+                    result = "Enter the System";
+                    break;
+                }
+            }
+
+            Console.WriteLine(result);
+            Console.Read();
+        }
+    }
+}
